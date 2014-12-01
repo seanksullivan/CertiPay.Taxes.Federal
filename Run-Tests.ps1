1838c6e1992b6dcb2a8f9b2808552a7df98799b9
Matt Wagner <mattgwagner@gmail.com>
2014-12-01 07:49:11 -0800

    Switch to a non-portable project for simplicity.
    Include only release binaries in the nuget package

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -1,9 +1,7 @@
 $ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 
-Set-Alias nunit "$(Join-Path $ScriptPath 'packages\NUnit.Runners.2.6.3\tools\nunit-console.exe.exe')"
+Set-Alias nunit "$(Join-Path $ScriptPath 'packages\NUnit.Runners.2.6.3\tools\nunit-console.exe')"
 
 $ProjectFile = Join-Path $ScriptPath "CertiPay.Taxes.Federal.Tests\bin\Debug\CertiPay.Taxes.Federal.Tests.dll"
-
-& "$ScriptPath\Build.ps1"
  
 nunit $ProjectFile
