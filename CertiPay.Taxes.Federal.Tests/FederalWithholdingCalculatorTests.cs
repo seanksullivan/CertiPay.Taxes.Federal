9d4400dfbd6046180f1502d1ac5b6d771f04c7ba
Matt Wagner <mattgwagner@gmail.com>
2014-12-01 16:08:37 -0500

    Add a few stub tests for calculating withholding

--- CertiPay.Taxes.Federal.Tests/FederalWithholdingCalculatorTests.cs
+++ CertiPay.Taxes.Federal.Tests/FederalWithholdingCalculatorTests.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using System;
 
 namespace CertiPay.Taxes.Federal.Tests
 {
     public class FederalWithholdingCalculatorTests
     {
-        // TODO
+        private readonly IFederalWithholdingCalculator _calculator = new FederalWithholdingCalculator { };
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Verify_Invalid_Year()
+        {
+            _calculator.Calculate(1965, 100);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Verify_Negative_Income()
+        {
+            _calculator.Calculate(2013, -10);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Verify_Negative_Allowances()
+        {
+            _calculator.Calculate(2013, 100, withholdingAllowances: -5);
+        }
     }
 }
--- CertiPay.Taxes.Federal/IFederalWithholdingCalculator.cs
+++ CertiPay.Taxes.Federal/IFederalWithholdingCalculator.cs
@@ -11,8 +11,32 @@ namespace CertiPay.Taxes.Federal
     {
         // TODO Calculate with allowances?
 
-        Decimal Calculate(int year, Decimal annualIncome, EmployeeTaxFilingStatus filingStatus = EmployeeTaxFilingStatus.Single, int withholdingAllownaces = 0);
+        Decimal Calculate(int year, Decimal annualIncome, EmployeeTaxFilingStatus filingStatus = EmployeeTaxFilingStatus.Single, int withholdingAllowances = 0);
     }
 
+    public class FederalWithholdingCalculator : IFederalWithholdingCalculator
+    {
+        public const int Minimum_Year = 2013;
 
+        public Decimal Calculate(int year, decimal annualIncome, EmployeeTaxFilingStatus filingStatus = EmployeeTaxFilingStatus.Single, int withholdingAllowances = 0)
+        {
+            if (year < Minimum_Year) throw new ArgumentOutOfRangeException("Unable to process tax years before " + Minimum_Year);
+
+            if (annualIncome < 0) throw new ArgumentOutOfRangeException("AnnualIncome cannot be negative");
+
+            if (withholdingAllowances < 0) throw new ArgumentOutOfRangeException("withholdingAllowances cannot be negative");
+
+            // Get the appropriate table by year and filing status
+
+            // Calculate the appropriate amount of withholdings based in annual income
+
+            // Subtract the appropriate amount based in withholding allowances
+
+            // Return the value
+
+            // TODO
+
+            return Decimal.Zero;
+        }
+    }
 }
