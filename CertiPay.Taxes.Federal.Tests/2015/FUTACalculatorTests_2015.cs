025e4666bcd6006ab74d51bdfa9da19354e499d8
Matt Wagner <mattgwagner@gmail.com>
2015-03-19 20:58:02 -0400

    Refactored unit tests into year-based folders

---
+++ CertiPay.Taxes.Federal.Tests/2015/FUTACalculatorTests_2015.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CertiPay.Taxes.Federal.Tests
+{
+    public class FUTACalculatorTests_2015
+    {
+        private readonly IFUTACalculator _calculator = new FUTACalculator();
+
+        private const int YEAR = 2015;
+
+        [Test]
+        [TestCase(5000, 300)]
+        [TestCase(7500, 420)]
+        [TestCase(99999, 420)]
+        public void Calculate_Medicare_Taxes(Decimal income, Decimal expectedTaxes)
+        {
+            var result = _calculator.Calculate(YEAR, income);
+
+            Assert.AreEqual(expectedTaxes, result);
+        }
+    }
+}
