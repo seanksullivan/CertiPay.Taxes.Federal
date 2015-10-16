9acf4ebf26c3bc6c59adc6db2f3a93bc06ab58b7
Matt Wagner <mattgwagner@gmail.com>
2015-10-16 10:40:13 -0400

    Missed updating tests for credit reduction application

--- CertiPay.Taxes.Federal.Tests/2014/FUTACalculatorTests_2014.cs
+++ CertiPay.Taxes.Federal.Tests/2014/FUTACalculatorTests_2014.cs
@@ -19,10 +19,10 @@ namespace CertiPay.Taxes.Federal.Tests
         }
 
         [Test]
-        [TestCase(5000, 300)]
-        [TestCase(7500, 420)]
-        [TestCase(99999, 420)]
-        public void Calculate_Medicare_Taxes(Decimal income, Decimal expectedTaxes)
+        [TestCase(5000, 30)]
+        [TestCase(7500, 42)]
+        [TestCase(99999, 42)]
+        public void Calculate_FUTA(Decimal income, Decimal expectedTaxes)
         {
             var result = _calculator.Calculate(YEAR, income);
 
