18f82044f7369bd671d8245e6f5a2a5bb10b1114
Matt Wagner <mattgwagner@gmail.com>
2016-12-05 09:53:18 -0500

    v1 of patching the project.json

---
+++ Update-Version.ps1
@@ -0,0 +1,16 @@
+param (
+	[string]$Version = $(throw "Version is required")
+)
+
+$Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$Projects = Get-ChildItem -Path $Here -Recurse -Include "project.json"
+
+foreach($Project in $Projects)
+{
+	$JSON = (Get-Content $Project) -join "`n" | ConvertFrom-Json
+
+	$JSON.version = $Version
+
+	ConvertTo-Json $JSON | Out-File $Project -Encoding utf8
+}
