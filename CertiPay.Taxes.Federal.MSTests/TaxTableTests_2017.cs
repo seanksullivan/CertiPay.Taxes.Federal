ba26ea87ec56c8330f5dc8d96799bf60785074c2
seanksullivan <seanksullivan@gmail.com>
2017-06-02 07:00:04 -0700

    Added test comments

--- CertiPay.Taxes.Federal.MSTests/TaxTableTests_2017.cs
+++ CertiPay.Taxes.Federal.MSTests/TaxTableTests_2017.cs
@@ -23,6 +23,12 @@ namespace CertiPay.Taxes.Federal.MSTests
             Assert.IsNotNull(calc.Calculate(YEAR, 1000));
         }
 
+        /// <summary>
+        /// MSTest2 example - using the DataRow atrribute, which is similar to nUnit's TestCase attribute - to provide data-driven unit test processing.
+        /// </summary>
+        /// <param name="annualIncome"></param>
+        /// <param name="status"></param>
+        /// <param name="expected"></param>
         [TestMethod]
         [DataRow(9325, EmployeeTaxFilingStatus.Single, 702.50)]
         [DataRow(85000, EmployeeTaxFilingStatus.Single, 16413.75)]
@@ -44,6 +50,10 @@ namespace CertiPay.Taxes.Federal.MSTests
             Assert.AreEqual(Convert.ToDecimal(expected), calc.Calculate(YEAR, annualIncome, status));
         }
 
+        /// <summary>
+        /// Example: Using the MSTest DataSource attribute functionality to provide the same "TestCase" functionality as nUnit,
+        /// but instead from a datasource like a Db, Excel, Xml, etc... to provide data-driven unit test processing.
+        /// </summary>
         [TestMethod]
         [DeploymentItem("TestData\\WithHolding.xml")]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "|DataDirectory|\\WithHolding.xml", "WithHolding", DataAccessMethod.Sequential)]
